7ed50c9b278bfaf762c9f9c3538f91510f98ecb3
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 10:59:56 +0530

    first commit

---
+++ FirstASPDotNetCore_WebAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstASPDotNetCore_WebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
